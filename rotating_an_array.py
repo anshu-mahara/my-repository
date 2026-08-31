1c1db5d2f621f5108220f349596b6934ba55717e
Anshu Mahara <anshu.mahara44@gmail.com>
2026-08-31 09:33:15 +0530

    Add rotate Array

--- rotating_an_array.py
+++ rotating_an_array.py
@@ -1,1 +1,7 @@
-# Nothing for now
+nums = [5, -2, 3, 9, 0, 6, 10, 7]
+# Here k is the number of indexes to be rotated to the right.
+k= 3
+n= len(nums)
+k= k%n
+nums[:]= nums[n-k: n] + nums[0:n-k]
+print(nums)
